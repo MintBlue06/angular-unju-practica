bcb791c4d606e18534ac2ed886ccb54330fee435
Mario Rios <mario300401@gmail.com>
2026-05-03 15:57:26 -0300

    feat: implementación data binding con ngModel

--- src/app/components/parte2/parte2.ts
+++ src/app/components/parte2/parte2.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parte2',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './parte2.html',
   styleUrl: './parte2.css',
 })
-export class Parte2 {}
+export class Parte2 {
+
+inscripcion = {
+  dni: '',
+  precio: 0,
+  categoriaAlumno: 0,
+  fechaInscripcion: '',
+  email: '',
+  curso: '',
+  total: 0
+};
+
+}
